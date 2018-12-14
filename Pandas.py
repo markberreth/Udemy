7fddf5500fec39fcef5974905f28b84fa01be3ff
Mark Berreth <mark.berreth@gmail.com>
2018-12-14 15:22:32 -0800

    Started course. Taking a break to review datetime at DataCamp.

--- Pandas.py
+++ Pandas.py
@@ -2,3 +2,22 @@
 Data Analysis with Pandas and Python
 '''
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+pokemon = pd.read_csv('/Users/markberreth/PycharmProjects/Udemy/pandas/pokemon.csv')
+
+print(pokemon.describe())
+
+googlestock = pd.read_csv('/Users/markberreth/PycharmProjects/Udemy/pandas/google_stock_price.csv')
+
+print(googlestock.describe())
+
+plt.plot(googlestock)
+plt.show()
+
+plt.hist(googlestock['Stock Price'])
+plt.show()
+
+print(googlestock.info())
