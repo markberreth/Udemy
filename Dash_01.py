865d2c273a5341e41f9fdb632a4d8ab46d44ecc9
Mark Berreth <mark.berreth@gmail.com>
2020-11-05 14:51:21 -0800

    Starting up course to learn Dash.

--- Dash_01.py
+++ Dash_01.py
@@ -1,14 +1,25 @@
 import numpy as np
 import pandas as pd
+import plotly
+import plotly.graph_objs as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 app = dash.Dash()
 
+server = app.server
+
+apple = pd.read_csv('')
+
 app.layout = html.Div(children=[
-    html.H1('Dash')
+    html.H1('Apple Stock Price')
     html.Div('Dash: Dashboard with Python')
+    dcc.Graph(id='apple',
+              figure={'data': apple,
+                      'layout': {'title':'Bar Plots'
+                                 }})
 ])
 
 if __name__ == '__main__':
